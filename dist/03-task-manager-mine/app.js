55efaf7bfa91cb709c05e799924a839c92838cbc
Prince <this.is.kumar.prince@gmail.com>
2021-11-11 15:31:36 +0530

    03-task-manager-mine project started

--- dist/03-task-manager-mine/app.js
+++ dist/03-task-manager-mine/app.js
@@ -1,2 +1,28 @@
 "use strict";
-console.log("hi");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const connect_1 = __importDefault(require("./db/connect"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const process_1 = require("process");
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const port = 3000;
+// Middleware
+app.use(express_1.default.json());
+app.use(express_1.default.static(path_1.default.resolve(__dirname, "./public")));
+const start = async () => {
+    try {
+        await (0, connect_1.default)(process_1.env.MONGO_URI);
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}....`);
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
+};
+start();
--- src/03-task-manager-mine/app.ts
+++ src/03-task-manager-mine/app.ts
@@ -1,1 +1,33 @@
-console.log("hi");
+import express from "express";
+import path from "path";
+import connect from "./db/connect";
+import dotenv from "dotenv";
+import { env } from "process";
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+declare global {
+  namespace NodeJS {
+    export interface ProcessEnv {
+      MONGO_URI: string;
+    }
+  }
+}
+
+// Middleware
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "./public")));
+
+const start = async () => {
+  try {
+    await connect(env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}....`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
