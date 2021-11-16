df90cd0bc3c538e7e9eccebfc01da5ad9dc41c1f
Prince <this.is.kumar.prince@gmail.com>
2021-11-16 17:10:16 +0530

    sign up form ready

--- dist/person-info/public/index.js
+++ dist/person-info/public/index.js
@@ -1,2 +1,20 @@
 "use strict";
-console.log("hi");
+/**
+ * All Variables
+ */
+const signUpBtn = document.querySelector(".signUp");
+// Remove login Token
+const removeLoginToken = () => {
+    localStorage.removeItem("login-token");
+};
+window.addEventListener("load", () => {
+    let loginToken = localStorage.getItem("login-token");
+    if (loginToken) {
+        signUpBtn.classList.add("account");
+        signUpBtn.innerHTML = `<i class="fas fa-user-circle"></i>`;
+    }
+    else {
+        signUpBtn.classList.remove("account");
+        signUpBtn.textContent = "sign up";
+    }
+});
