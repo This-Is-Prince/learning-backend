ba4da72a2d61ce7cb1b3062cdfe6d708450bc403
Prince <this.is.kumar.prince@gmail.com>
2021-11-20 19:29:07 +0530

    06-jobs-api completed

--- dist/06-jobs-api/app.js
+++ dist/06-jobs-api/app.js
@@ -7,6 +7,12 @@ require("express-async-errors");
 const process_1 = require("process");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
+// extra security Packages
+const helmet_1 = __importDefault(require("helmet"));
+const cors_1 = __importDefault(require("cors"));
+// import * as xss from 'xss';
+const xss_clean_1 = __importDefault(require("xss-clean"));
+const express_rate_limit_1 = __importDefault(require("express-rate-limit"));
 const not_found_1 = __importDefault(require("./middleware/not-found"));
 const error_handler_1 = __importDefault(require("./middleware/error-handler"));
 const express_1 = __importDefault(require("express"));
@@ -21,6 +27,14 @@ const app = (0, express_1.default)();
 const port = process_1.env.PORT || 3000;
 // General Middleware
 app.use(express_1.default.json());
+app.use((0, helmet_1.default)());
+app.use((0, cors_1.default)());
+app.use((0, xss_clean_1.default)());
+app.set("trust proxy", 1);
+app.use((0, express_rate_limit_1.default)({
+    windowMs: 15 * 60 * 1000,
+    max: 100, //limit each IP to 100 request per windowMs
+}));
 // All Routes
 app.use("/api/v1/auth", auth_1.default);
 app.use("/api/v1/jobs", authentication_1.default, jobs_1.default);
--- src/06-jobs-api/app.ts
+++ src/06-jobs-api/app.ts
@@ -2,6 +2,14 @@ require("express-async-errors");
 import { env } from "process";
 import dotenv from "dotenv";
 dotenv.config();
+
+// extra security Packages
+import helmet from "helmet";
+import cors from "cors";
+// import * as xss from 'xss';
+import xss from "xss-clean";
+import rateLimiter from "express-rate-limit";
+
 import notFound from "./middleware/not-found";
 import errorHandlerMiddleware from "./middleware/error-handler";
 import express from "express";
@@ -30,6 +38,16 @@ const port = env.PORT || 3000;
 
 // General Middleware
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, //15 minutes
+    max: 100, //limit each IP to 100 request per windowMs
+  })
+);
 
 // All Routes
 app.use("/api/v1/auth", authRouter);
