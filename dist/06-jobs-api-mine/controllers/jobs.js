70bc8e16acd422dd81eab02cac1c6436bda6299a
Prince <this.is.kumar.prince@gmail.com>
2021-11-23 15:55:36 +0530

    jobs all routes done

--- dist/06-jobs-api-mine/controllers/jobs.js
+++ dist/06-jobs-api-mine/controllers/jobs.js
@@ -1,23 +1,61 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getJob = exports.updateJob = exports.getAllJobs = exports.deleteJob = exports.createJob = void 0;
+const Job_1 = __importDefault(require("../models/Job"));
+const http_status_codes_1 = require("http-status-codes");
+const errors_1 = require("../errors");
 const getAllJobs = async (req, res) => {
-    res.send("get all jobs");
+    const jobs = await Job_1.default.find({ createdBy: req.user.userId }).sort("createdAt");
+    res.status(http_status_codes_1.StatusCodes.OK).json({
+        msg: "Successfully finds all jobs...",
+        data: { jobs, count: jobs.length },
+    });
 };
 exports.getAllJobs = getAllJobs;
-const getJob = async (req, res) => {
-    res.send("get job");
-};
-exports.getJob = getJob;
 const createJob = async (req, res) => {
-    res.json(req.user);
+    req.body.createdBy = req.user.userId;
+    const job = await Job_1.default.create(req.body);
+    res
+        .status(http_status_codes_1.StatusCodes.CREATED)
+        .json({ msg: "Successfully created...", data: { job } });
 };
 exports.createJob = createJob;
+const getJob = async (req, res) => {
+    const { user: { userId }, params: { id: jobId }, } = req;
+    const job = await Job_1.default.findOne({ createdBy: userId, _id: jobId });
+    if (!job) {
+        throw new errors_1.NotFoundError(`No job with id ${jobId}...`);
+    }
+    res
+        .status(http_status_codes_1.StatusCodes.OK)
+        .json({ msg: "Successfully find a job...", data: { job } });
+};
+exports.getJob = getJob;
 const updateJob = async (req, res) => {
-    res.send("update job");
+    const { body: { company, position }, user: { userId }, params: { id: jobId }, } = req;
+    if (company === "" || position === "") {
+        throw new errors_1.BadRequest("Company or Position fields cannot be empty...");
+    }
+    const job = await Job_1.default.findByIdAndUpdate({ _id: jobId, createdBy: userId }, req.body, { new: true, runValidators: true });
+    if (!job) {
+        throw new errors_1.NotFoundError(`No job with id ${jobId}...`);
+    }
+    res
+        .status(http_status_codes_1.StatusCodes.OK)
+        .json({ msg: "Successfully update a job...", data: { job } });
 };
 exports.updateJob = updateJob;
 const deleteJob = async (req, res) => {
-    res.send("delete job");
+    const { user: { userId }, params: { id: jobId }, } = req;
+    const job = await Job_1.default.findOneAndRemove({ createdBy: userId, _id: jobId });
+    if (!job) {
+        throw new errors_1.NotFoundError(`No job with id ${jobId}...`);
+    }
+    res
+        .status(http_status_codes_1.StatusCodes.OK)
+        .json({ msg: "Successfully delete a job...", data: { job } });
 };
 exports.deleteJob = deleteJob;
--- src/06-jobs-api-mine/controllers/jobs.ts
+++ src/06-jobs-api-mine/controllers/jobs.ts
@@ -1,23 +1,89 @@
-import { RequestHandler } from "express";
+import { RequestHandler, Request } from "express";
+import Job from "../models/Job";
+import { StatusCodes } from "http-status-codes";
+import { BadRequest, NotFoundError } from "../errors";
+
+interface CreateJobRequest extends Request {
+  body: {
+    company: string;
+    position: string;
+    createdBy: string;
+  };
+}
+interface UpdateJobRequest extends Request {
+  body: {
+    company: string;
+    position: string;
+  };
+}
 
 const getAllJobs: RequestHandler = async (req, res) => {
-  res.send("get all jobs");
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+  res.status(StatusCodes.OK).json({
+    msg: "Successfully finds all jobs...",
+    data: { jobs, count: jobs.length },
+  });
+};
+
+const createJob: RequestHandler = async (req: CreateJobRequest, res) => {
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res
+    .status(StatusCodes.CREATED)
+    .json({ msg: "Successfully created...", data: { job } });
 };
 
 const getJob: RequestHandler = async (req, res) => {
-  res.send("get job");
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+  const job = await Job.findOne({ createdBy: userId, _id: jobId });
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}...`);
+  }
+
+  res
+    .status(StatusCodes.OK)
+    .json({ msg: "Successfully find a job...", data: { job } });
 };
 
-const createJob: RequestHandler = async (req, res) => {
-  res.json(req.user);
-};
+const updateJob: RequestHandler = async (req: UpdateJobRequest, res) => {
+  const {
+    body: { company, position },
+    user: { userId },
+    params: { id: jobId },
+  } = req;
 
-const updateJob: RequestHandler = async (req, res) => {
-  res.send("update job");
+  if (company === "" || position === "") {
+    throw new BadRequest("Company or Position fields cannot be empty...");
+  }
+  const job = await Job.findByIdAndUpdate(
+    { _id: jobId, createdBy: userId },
+    req.body,
+    { new: true, runValidators: true }
+  );
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}...`);
+  }
+  res
+    .status(StatusCodes.OK)
+    .json({ msg: "Successfully update a job...", data: { job } });
 };
 
 const deleteJob: RequestHandler = async (req, res) => {
-  res.send("delete job");
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+  const job = await Job.findOneAndRemove({ createdBy: userId, _id: jobId });
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}...`);
+  }
+
+  res
+    .status(StatusCodes.OK)
+    .json({ msg: "Successfully delete a job...", data: { job } });
 };
 
 export { createJob, deleteJob, getAllJobs, updateJob, getJob };
